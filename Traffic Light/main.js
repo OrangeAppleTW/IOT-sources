2b8c8965233cf77672ab15a19e8bf6d47ec995cf
Barney <arthurlu@outlook.com>
2015-11-05 18:17:27 +0800

    Fix buzzer bug

--- Traffic Light/main.js
+++ Traffic Light/main.js
@@ -1,20 +1,21 @@
 function buttonHandler(){
-    led_R.on();
-    led_Y.off();
-    led_G.off();
-    light.Red = 'on';
-    light.Yellow = 'off';
-    light.Green = 'off';
-  setTimeout(function(){
-    led_Y.on();
-    light.Yellow = 'on';
-  }, 2000);
-  setTimeout(function(){
-    led_G.on();
-    led_R.off();
-    led_Y.off();
-    light.Red = 'off';
-    light.Yellow = 'off';
-    light.Green = 'on';
-  }, 4000);
+  console.log('成功！');
+  //   led_R.on();
+  //   led_Y.off();
+  //   led_G.off();
+  //   light.Red = 'on';
+  //   light.Yellow = 'off';
+  //   light.Green = 'off';
+  // setTimeout(function(){
+  //   led_Y.on();
+  //   light.Yellow = 'on';
+  // }, 2000);
+  // setTimeout(function(){
+  //   led_G.on();
+  //   led_R.off();
+  //   led_Y.off();
+  //   light.Red = 'off';
+  //   light.Yellow = 'off';
+  //   light.Green = 'on';
+  // }, 4000);
 };
--- js/setup.js
+++ js/setup.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   var load = $.Deferred(function(promise) {
     // https://rawgit.com/OrangeAppleTW/IOT-sources/master/lib/head.html
     // ../lib/head.html
-    $("head").load("https://rawgit.com/OrangeAppleTW/IOT-sources/master/lib/head.html", function(response, status, xhr){
+    $("head").load("../lib/head.html", function(response, status, xhr){
       if (status == 'success'){
         // Register variables
         $('[id]').each(function(){
@@ -54,9 +54,8 @@ $(document).ready(function(){
       window.addEventListener('WebComponentsReady', function() {
         $(board).on('ready', function(){
           $('wa-button').each(function(){
-            this.on('pressed', buttonHandler);
-          });
-          main();
+            $(this).on('pressed', buttonHandler);
+          })
         })
       });
       console.log('Success');})
