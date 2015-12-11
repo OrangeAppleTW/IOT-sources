edd80bef5d55fc4ffa8483da6bb704970ceda1f0
Barney <arthurlu@outlook.com>
2015-12-11 18:00:29 +0800

    Directory: change the name 'Traffic Light' to 'Traffic-Light'

---
+++ Traffic-Light/main.js
@@ -0,0 +1,29 @@
+function buttonHandler(){
+  console.log('成功！');
+    led_R.on();
+    led_Y.off();
+    led_G.off();
+    light.Red = 'on';
+    light.Yellow = 'off';
+    light.Green = 'off';
+  setTimeout(function(){
+    led_Y.on();
+    light.Yellow = 'on';
+  }, 1000);
+  setTimeout(function(){
+    led_R.off();
+    led_Y.off();
+    led_G.on();
+    light.Red = 'off';
+    light.Yellow = 'off';
+    light.Green = 'on';
+  }, 2000);
+  setTimeout(function(){
+    led_R.off();
+    led_Y.off();
+    led_G.off();
+    light.Red = 'off';
+    light.Yellow = 'off';
+    light.Green = 'off';
+  }, 3000);
+};
