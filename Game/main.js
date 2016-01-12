b6f92a591ad7a3708b2dd859a34d0dd692b7f36f
Barney <arthurlu@outlook.com>
2016-01-12 12:59:46 +0800

    Add Projects: Game

--- Game/main.js
+++ Game/main.js
@@ -1,20 +1,3 @@
-function main(){
-  if (game.state == 'stop'){
-    if (game.score > game.highestScore){
-      // led1.on();
-      // led2.off();
-    }
-    else {
-      // led1.on();
-      // led2.on();
-    }
-  }
-  else if (game.state == 'playing'){
-    // led1.off();
-    // led2.on();
-  }
-};
-
 function buttonHandler(){
   if (game.state == 'stop'){
     game.start();
